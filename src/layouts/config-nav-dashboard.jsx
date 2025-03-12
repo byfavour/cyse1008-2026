6a703789ec9f6c6f9f39fc5109591e70310d867b
cliff <dunderjeep@gmail.com>
2025-03-12 08:38:23 -0400

    updating images

--- src/layouts/config-nav-dashboard.jsx
+++ src/layouts/config-nav-dashboard.jsx
@@ -48,6 +48,7 @@ export const navData = [
   {
     subheader: 'Overview',
     items: [
+      { title: 'Shop', path: paths.dashboard.general.encryption, icon: ICONS.analytics },
       { title: 'Encryption', path: paths.dashboard.general.encryption, icon: ICONS.analytics },
       { title: 'App', path: paths.dashboard.root, icon: ICONS.dashboard },
       { title: 'Ecommerce', path: paths.dashboard.general.ecommerce, icon: ICONS.ecommerce },
@@ -173,7 +174,7 @@ export const navData = [
         title: 'Permission',
         path: paths.dashboard.permission,
         icon: ICONS.lock,
-        roles: ['admin', ],
+        roles: ['admin'],
         caption: 'Only admin can see this item',
       },
       {
--- src/sections/home/home-hero-cyse1008.jsx
+++ src/sections/home/home-hero-cyse1008.jsx
@@ -6,50 +6,49 @@ import { useTheme } from '@mui/material/styles';
 
 import { textGradient } from 'src/theme/styles';
 
-
 const lgKey = 'lg';
 
 export function HomeHeroCYSE1008() {
   const theme = useTheme();
   return (
-  <Stack alignItems="center" spacing={2.5}>
-    <Box
-      component="h1"
-      display="flex"
-      flexWrap="wrap"
-      justifyContent="center"
-      sx={{
-        ...theme.typography.h2,
-        my: 0,
-        mx: 'auto',
-        maxWidth: 680,
-        fontFamily: theme.typography.fontSecondaryFamily,
-        [theme.breakpoints.up(lgKey)]: { fontSize: 72, lineHeight: '90px' },
-      }}
-    >
+    <Stack alignItems="center" spacing={2.5}>
       <Box
-        component={m.span}
-        animate={{ backgroundPosition: '200% center' }}
-        transition={{
-          duration: 20,
-          ease: 'linear',
-          repeat: Infinity,
-          repeatType: 'reverse',
-        }}
+        component="h1"
+        display="flex"
+        flexWrap="wrap"
+        justifyContent="center"
         sx={{
-          ...textGradient(
-            `300deg, ${theme.vars.palette.primary.main} 0%, ${theme.vars.palette.warning.main} 25%, ${theme.vars.palette.primary.main} 50%, ${theme.vars.palette.warning.main} 75%, ${theme.vars.palette.primary.main} 100%`
-          ),
-          backgroundSize: '400%',
-          ml: { xs: 0.75, md: 1, xl: 1.5 },
+          ...theme.typography.h2,
+          my: 0,
+          mx: 'auto',
+          maxWidth: 680,
+          fontFamily: theme.typography.fontSecondaryFamily,
+          [theme.breakpoints.up(lgKey)]: { fontSize: 72, lineHeight: '90px' },
         }}
       >
-        Welcome to Quilt
+        <Box
+          component={m.span}
+          animate={{ backgroundPosition: '200% center' }}
+          transition={{
+            duration: 20,
+            ease: 'linear',
+            repeat: Infinity,
+            repeatType: 'reverse',
+          }}
+          sx={{
+            ...textGradient(
+              `300deg, ${theme.vars.palette.primary.main} 0%, ${theme.vars.palette.warning.main} 25%, ${theme.vars.palette.primary.main} 50%, ${theme.vars.palette.warning.main} 75%, ${theme.vars.palette.primary.main} 100%`
+            ),
+            backgroundSize: '400%',
+            ml: { xs: 0.75, md: 1, xl: 1.5 },
+          }}
+        >
+          Welcome to CYSE-1008
+        </Box>
       </Box>
-    </Box>
-    {/* <ExtendedEuclideanAlgorithm />
+      {/* <ExtendedEuclideanAlgorithm />
     <ModularExponentiation />
     <EulerTotientFunction /> */}
-  </Stack>
+    </Stack>
   );
 }
