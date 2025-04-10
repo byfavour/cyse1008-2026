b56ca65c6b16e06ab9f45d8292e9ba9f6893fbcc
cliff <dunderjeep@gmail.com>
2025-04-10 11:59:22 -0400

    added firebase functions for stripe

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const { onRequest } = require('firebase-functions/v2/https');
+const logger = require('firebase-functions/logger');
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +17,38 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const admin = require('firebase-admin');
+const Stripe = require('stripe');
+
+admin.initializeApp();
+// const db = admin.firestore();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2023-10-16' });
+
+exports.handleStripeWebhook = onRequest(async (req, res) => {
+  const sig = req.headers['stripe-signature'];
+  const event = stripe.webhooks.constructEvent(req.rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET);
+
+  if (event.type === 'payment_intent.succeeded') {
+    const intent = event.data.object;
+
+    // Split transfer logic
+    const vendorSplits = [
+      { id: 'acct_vendor1', amount: 400 },
+      { id: 'acct_vendor2', amount: 300 },
+    ];
+
+    await Promise.all(
+      vendorSplits.map((split) =>
+        stripe.transfers.create({
+          amount: split.amount,
+          currency: 'cad',
+          destination: split.id,
+          transfer_group: intent.metadata.transfer_group,
+        })
+      )
+    );
+  }
+
+  res.sendStatus(200);
+});
