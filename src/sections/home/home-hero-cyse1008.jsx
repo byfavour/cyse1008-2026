03009b929974f0d96dd8caa9779c72aff76985af
cliff <dunderjeep@gmail.com>
2025-08-26 13:52:55 +0200

    hero pre save

--- src/sections/home/home-hero-cyse1008.jsx
+++ src/sections/home/home-hero-cyse1008.jsx
@@ -25,28 +25,7 @@ export function HomeHeroCYSE1008() {
           fontFamily: theme.typography.fontSecondaryFamily,
           [theme.breakpoints.up(lgKey)]: { fontSize: 72, lineHeight: '90px' },
         }}
-      >
-        <Box
-          component={m.span}
-          animate={{ backgroundPosition: '200% center' }}
-          transition={{
-            duration: 20,
-            ease: 'linear',
-            repeat: Infinity,
-            repeatType: 'reverse',
-          }}
-          sx={{
-            ...textGradient(
-              `300deg, ${theme.vars.palette.primary.main} 0%, ${theme.vars.palette.warning.main} 25%, ${theme.vars.palette.primary.main} 50%, ${theme.vars.palette.warning.main} 75%, ${theme.vars.palette.primary.main} 100%`
-            ),
-            backgroundSize: '400%',
-            ml: { xs: 0.75, md: 1, xl: 1.5 },
-            textAlign: 'center',
-          }}
-        >
-          CYSE-1008
-        </Box>
-      </Box>
+      ></Box>
       <Box
         component="h1"
         display="flex"
@@ -79,10 +58,9 @@ export function HomeHeroCYSE1008() {
             textAlign: 'center',
           }}
         >
-          eCommerce
+          Patchwork
           <br />
-          & <br />
-          Cybersecurity
+          Local eCommerce
         </Box>
       </Box>
       {/* <ExtendedEuclideanAlgorithm />
