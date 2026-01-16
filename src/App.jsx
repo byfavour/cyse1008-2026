944faef4ce4c4bbfe434113be076756789313d05
Matthew <matthew@pop-os.localdomain>
2026-01-16 09:20:24 -0500

    Assignment 1: Added my name

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR.  - "Hello World!"
+          Edit <code>src/App.jsx</code> and save to test HMR.  - "Hello Matthew Badzik!"
         </p>
       </div>
       <p className="read-the-docs">
