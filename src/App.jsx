bca323f8ab5c96115d07c61aaa1e43d4c4c3fc57
byfavour <favouriheanyieboh@loyalistcollege.com>
2026-01-30 09:32:11 -0500

    Assignment 1: added my name and told myself to lock in

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR.  - "Hello Matthew Badzik!"
+          Edit <code>src/App.jsx</code> and save to test HMR.  - "Hello Favour Eboh, you need to get your head in the game, you are behind!"
         </p>
       </div>
       <p className="read-the-docs">
